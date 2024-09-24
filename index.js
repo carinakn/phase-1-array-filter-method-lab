8a5318d7b388fd355cdb276ebc4035e368cd77f7
carinakn <carinaknics@gmail.com>
2024-09-24 14:56:39 -0400

    complete lab 2

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function findMatching(drivers, name) {
     return drivers.filter(driver => driver.name === name);
   }
   
+  const drivers = [
+    { name: "Alice", hometown: "Seattle" },
+    { name: "Bob", hometown: "New York" },
+    { name: "Charlie", hometown: "Los Angeles" },
+    { name: "alice", hometown: "Chicago" },
+    { name: "Bobby", hometown: "Miami" }
+  ];
+  
+  const driverNames = ["Alice", "Bob", "Charlie", "alice", "Bobby"];
